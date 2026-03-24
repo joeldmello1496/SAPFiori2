e53cc242091a6584b8f77fb16bf434f13fe405ee
Joel Dmello <joel.dmello@castaliaz.co.in>
2026-03-24 09:35:17 +0000

    second Change

---
+++ zfcastaliazprolifics/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("zfcastaliazprolifics.controller.View1", {
+        onInit() {
+        }
+    });
+});
